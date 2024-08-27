aef3ab7033f5ddf115360a97e661f6168032788b
Hemanth5603 <shemanth2003.vskp@gmail.com>
2024-08-27 11:07:19 +0530

    Production setting config change

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def audiototext(incommingFile):
 
 if __name__ == '__main__':
     # Bind to the port specified by the PORT environment variable
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
     #audiototext()
